1448fac9dbc92602a3ab589518050c5294b1db6d
Local User <you@local>
2026-05-09 16:35:40 +0800

    removed useless files

--- add_pending_data_column.sql
+++
@@ -1,4 +0,0 @@
--- Add pending_data column to users table to store registration information
--- until admin approves the member/trainer
-
-ALTER TABLE users ADD COLUMN pending_data TEXT NULL AFTER is_active;
--- add_program_data_column.sql
+++
@@ -1,4 +0,0 @@
--- Add program_data column to member_fitness_goals table
-ALTER TABLE member_fitness_goals 
-ADD COLUMN program_data TEXT DEFAULT NULL
-AFTER fitness_goal;
